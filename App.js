86cc6875234e4d59a8ab7d4ce3e851bd9e86ffc1
Sharon Sujitha <86667113+sharonsujitha7@users.noreply.github.com>
2023-08-16 01:23:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+//import logo from './logo.svg';
+import './App.css';
+import './Login';
+import SignIn from './Login';
+import CreateAccount from './CreateAccount';
+
+function App() {
+  return (
+    <div className="App">
+      <SignIn></SignIn>
+      <CreateAccount></CreateAccount>
+      
+    </div>
+  );
+}
+
+export default App;
